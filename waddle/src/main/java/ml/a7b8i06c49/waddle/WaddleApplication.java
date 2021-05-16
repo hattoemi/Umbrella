872c5ba5db14ff3b392c1989c8c53a2cba3ff05c
Hattori-emi <a7b8i06c49@outlook.com>
2021-05-16 14:57:18 +0800

    enable windows desktop

--- waddle/src/main/java/ml/a7b8i06c49/waddle/WaddleApplication.java
+++ waddle/src/main/java/ml/a7b8i06c49/waddle/WaddleApplication.java
@@ -3,6 +3,9 @@ package ml.a7b8i06c49.waddle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author hattoriemi
+ */
 @SpringBootApplication
 public class WaddleApplication {
 
